10ed031c85c4b14e883fe6e9e9df89d063884901
marcolin94 <mranica@sorint.it>
2019-04-19 00:25:31 +0200

    Organizzazione import delle constanti

--- lambda/custom/lib/constants/messages.js
+++ lambda/custom/lib/constants/messages.js
@@ -1,6 +1,7 @@
 const messages = {
     ERROR: 'Mi dispiace, si è verificato un errore.',
     STOP: 'Ciao, alla prossima!',
+    MORE_INFO : 'cittadino, desidera sapere altro?'
 };
 
 module.exports = messages;
--- lambda/custom/lib/handlers/invocation.js
+++ lambda/custom/lib/handlers/invocation.js
@@ -1,11 +1,19 @@
+const moment = require('moment');
+const message = require('./../constants/messages');
+const calendar = require('./../constants/calendar');
+
 const InvocationHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speechText = 'Ciao cittadino, cosa desideri sapere?';
+        const today = moment.moment().format('MM/DD/YYYY');
+        console.log(today);
+        const rifiutoGiornalierio = calendar.get(today);
+        console.log(rifiutoGiornalierio);
+
         return handlerInput.responseBuilder
-            .speak(speechText)
+            .speak(rifiutoGiornalierio)
             .reprompt(speechText)
             .getResponse();
     }
