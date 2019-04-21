84a2fe62c01931e53c0c885c87611a1283f4234b
marcolin94 <mranica@sorint.it>
2019-04-21 18:55:03 +0200

    added messages and deleted console.log

--- lambda/custom/lib/handlers/invocation.js
+++ lambda/custom/lib/handlers/invocation.js
@@ -1,36 +1,27 @@
-const message = require('./../constants/messages');
+const messages = require('./../constants/messages');
 const dateMethods = require('./../method/dateMethods');
 const trashMethods = require('./../method/trashMethods');
 
 const InvocationHandler = {
     canHandle(handlerInput) {
-        console.log("ERROR00" + handlerInput.requestEnvelope.request.type);
-
         const request = handlerInput.requestEnvelope.request;
         return request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
         const today = dateMethods.getNowDate();
-        console.log("today "+ today);
-
         const listCodesTrash = dateMethods.getRifiutidelGiorno('27/09/2019');
-        console.log("rifiutiGiornalierio "+ listCodesTrash);
-        console.log( "Type rifiuti : " + typeof listCodesTrash)
-
         const dateToSpeech = dateMethods.getFormatForSpeech(today);
-        console.log("dateToSpeech "+ dateToSpeech);
 
         let SPEECH = "";
         if (listCodesTrash.size === 0) {
-            SPEECH = message.NO_RIFIUTI + dateToSpeech;
+            SPEECH = messages.NO_RIFIUTI + dateToSpeech;
         } else {
             const trashToSpeech = trashMethods.getListOfTrash(listCodesTrash);
-            console.log("rifiuti "+ trashToSpeech);
-            SPEECH = message.RIFIUTI + dateToSpeech + " sono: " + trashToSpeech;
+            SPEECH = messages.RIFIUTI + dateToSpeech + " sono: " + trashToSpeech + ". " + messages.MORE_INFO;
         }
         return handlerInput.responseBuilder
             .speak(SPEECH)
-            .reprompt("speechText")
+            .withShouldEndSession(false)
             .getResponse();
     },
 };
