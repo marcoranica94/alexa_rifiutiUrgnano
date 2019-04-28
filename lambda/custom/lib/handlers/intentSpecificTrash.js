cd53577dee73bed429e089bb9c38de48279f0d1d
marcolin94 <94marculi@gmail.com>
2019-04-29 00:59:21 +0200

    completed intentSpecificTrash

--- lambda/custom/lib/handlers/intentSpecificTrash.js
+++ lambda/custom/lib/handlers/intentSpecificTrash.js
@@ -1,4 +1,9 @@
 const message = require('../constants/messages');
+const calendar = require('../constants/calendar');
+const dateMethods = require('./../method/dateMethods');
+const genericMethods = require('./../method/genericMethods');
+const trashMethods = require('./../method/trashMethods');
+const rifiutiCodeDetails = require('../constants/rifiutiCodeDetails');
 
 const SpecificTrashHandler = {
     canHandle(handlerInput) {
@@ -7,8 +12,46 @@ const SpecificTrashHandler = {
             && request.intent.name === 'SpecificTrashIntent';
     },
     handle(handlerInput) {
+        const slots = handlerInput.requestEnvelope.request.intent.slots;
+        trashSlot = slots['SlotTrash'] || "";
+        const idTrash = trashSlot.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+
+        if(trashSlot === "" || idTrash === undefined){
+            return handlerInput.responseBuilder
+                .speak(message.UNKNOWN_TRASH)
+                .withShouldEndSession(false)
+                .getResponse();
+        }
+        
+        const today = dateMethods.getNowDate();
+        let indexKey = -1;
+        let date = today;
+        do {
+            indexKey = genericMethods.findIndexFromMap(calendar, date);
+            date = dateMethods.addDaysFromDate(date, 1);
+        } while (indexKey < 0)
+
+        const sliceMap = genericMethods.getSliceMap(calendar, indexKey);
+        const dateAndCodeTrash = trashMethods.findTrashAndDateFromSpecific(sliceMap, idTrash);
+
+        if(dateAndCodeTrash === undefined) {
+            return handlerInput.responseBuilder
+                .speak(message.NO_RIFIUTI_YEAR)
+                .withShouldEndSession(false)
+                .getResponse();
+        }
+        const listCodeTrashWithoutTrashSlot = dateAndCodeTrash[1].filter(x => x !== idTrash)
+        const listTrash = trashMethods.getListOfTrash(listCodeTrashWithoutTrashSlot);
+        const dateSpeech = dateMethods.getFormatForSpeech(dateAndCodeTrash[0]);
+        let speech = message.SPECIFIC_TRASH(rifiutiCodeDetails[idTrash], dateSpeech);
+
+        if(listTrash.size !== '') {
+            speech = speech + ' insieme a : ' + listTrash;
+        }
+
         return handlerInput.responseBuilder
-            .speak(message.STOP)
+            .speak(speech)
+            .withShouldEndSession(false)
             .getResponse();
     },
 };
