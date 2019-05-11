971ff3a416ad3d50baf2ea38d39718d415d99651
marcolin94 <94marculi@gmail.com>
2019-05-11 13:33:55 +0200

    modified message and removed unsed intent

--- lambda/custom/lib/constants/messages.js
+++ lambda/custom/lib/constants/messages.js
@@ -2,7 +2,7 @@ const messages = {
     ERROR: 'Mi dispiace, si è verificato un errore.',
     STOP: 'Ciao, alla prossima!',
     MORE_INFO : 'Cittadino, desidera sapere altro?',
-    NO_RIFIUTI : 'Non è previsto il ritiro dei rifiuti oggi, il ',
+    NO_RIFIUTI : 'Non è previsto il ritiro dei rifiuti la mattina seguente, il ',
     RIFIUTI : 'I rifiuti che verranno ritirati il ',
     UNKNOWN_TRASH: 'Non conosco questo rifiuto',
     NO_RIFIUTI_YEAR: 'Non è previsto il ritiro quest anno',
